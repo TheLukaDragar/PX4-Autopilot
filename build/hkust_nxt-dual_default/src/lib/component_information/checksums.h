f72736436f285e8b248a5d3747c8fc61e43e00d0
Luka Dragar <luka.dragar@ssrd.io>
2025-09-02 14:59:41 +0200

    compile NxtPX4v2

---
+++ build/hkust_nxt-dual_default/src/lib/component_information/checksums.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <stdint.h>
+namespace component_information {
+static constexpr uint32_t component_general_crc = 3881515573;
+static constexpr const char *component_general_sha256 = "088bac00a743a4affd65fd83ccdcbdf52dcd3fbfec403d3859b62a9e54ac8e8d";
+static constexpr uint32_t all_events_crc = 3557846516;
+static constexpr const char *all_events_sha256 = "146985c6b15e8e054b7c6b5a0cce59df8502270cea2d058743feeedefe1a6140";
+}
